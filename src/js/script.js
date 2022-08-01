14fb7f0864aa9d24fbde0ec330682437954a9529
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-08-01 14:57:08 -0300

    realizando modificações realizadas pelo coach

--- src/js/script.js
+++ src/js/script.js
@@ -14,24 +14,24 @@ function criarCard(objeto) {
     tagLi.append(tagImg, tagH3, tagSpan, tagP);
     return tagLi;
 }
-function criarLista(callbackFn, thisArg) {
+function criarLista(thisArg) {
     const tagUl = document.querySelector(".containerListaProdutos ul");
     tagUl.innerHTML = "";
-    thisArg.forEach((element, index, array) => {
-        tagUl.appendChild(callbackFn(element));
+    thisArg.forEach((element) => {
+        tagUl.appendChild(criarCard(element));
     })
-    mostrarValorTotal(calcularValorTotal, thisArg);
+    mostrarValorTotal(thisArg);
 }
 function filtrarPorNome() {
     const tagInput = document.querySelector(".campoBuscaPorNome");
     let nomeBuscado = tagInput.value.toLowerCase();
-    let produtosFiltrados = produtos.filter((element, index, array) => {
+    let produtosFiltrados = produtos.filter((element) => {
         return nomeBuscado == element.nome.toLowerCase();
     });
     return produtosFiltrados;
 }
 function filtrarPorSecao(secao) {
-    let produtosFiltrados = produtos.filter((element, index, array) => {
+    let produtosFiltrados = produtos.filter((element) => {
         return secao == element.secao;
     });
     return produtosFiltrados;
@@ -39,25 +39,27 @@ function filtrarPorSecao(secao) {
 const botaoBuscaPorNome = document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome");
 botaoBuscaPorNome.addEventListener("click", () => {
     const novoProdutos = filtrarPorNome();
-    criarLista(criarCard, novoProdutos);
+    criarLista(novoProdutos);
 });
-const botoesContainer = document.querySelector("#botoesContainer");
-botoesContainer.addEventListener("click", (e) => {
-    const alvo = e.target;
-    if(alvo.innerText == "Todos Produtos") {
-        criarLista(criarCard, produtos);
-    } else {
-        const novoProdutos = filtrarPorSecao(`${e.target.innerText}`);
-        criarLista(criarCard, novoProdutos);
-    }
-});
+const botoesContainer = document.querySelectorAll("#botoesContainer button");
+botoesContainer.forEach((botao) => {
+    botao.addEventListener("click", (e) => {
+        const alvo = e.target;
+        if(alvo.innerText == "Todos Produtos") {
+            criarLista(produtos);
+        } else {
+            const novoProdutos = filtrarPorSecao(`${e.target.innerText}`);
+            criarLista(novoProdutos);
+        }
+    });
+})
 function calcularValorTotal(array) {
     let valorTotal = 0;
-    array.forEach((element, index, array) => {valorTotal += element.preco});
+    array.forEach((element) => {valorTotal += element.preco});
     return valorTotal;
 }
-function mostrarValorTotal(callbackFn, thisArg) {
+function mostrarValorTotal(thisArg) {
     let tagSpan = document.querySelector(".priceContainer span");
-    tagSpan.innerText = `R$ ${callbackFn(thisArg)}.00`;
+    tagSpan.innerText = `R$ ${calcularValorTotal(thisArg)}.00`;
 }
-criarLista(criarCard, produtos);
+criarLista(produtos);
